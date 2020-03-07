05345db0557038028c0b6486de9baa428eb44bf8
Kayla Melton <kannmelton2@gmail.com>
2020-03-07 11:30:44 -0600

    added styling

--- src/javascripts/components/studentCard/studentCard.js
+++ src/javascripts/components/studentCard/studentCard.js
@@ -1,8 +1,8 @@
 import './studentCard.scss';
 
 const createStudentCard = (student) => {
-  let domString = '<li>';
-  domString += '<div>';
+  let domString = '<li class="student-card">';
+  domString += '<div class="img-holder">';
   domString += `<img src="${student.picture}" alt=""/>`;
   domString += '</div>';
   domString += `<h2>${student.name}</h2>`;
