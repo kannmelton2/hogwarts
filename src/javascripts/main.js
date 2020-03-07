c74db1a50cb007d323cfab09b3f724a04b71fac1
Kayla Melton <kannmelton2@gmail.com>
2020-03-07 13:55:52 -0600

    added house buttons

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,14 @@
 import studentData from './helpers/data/studentData';
 import navbar from './components/navbar/navbar';
 import studentList from './components/studentList/studentList';
+import buttonGroup from './components/buttonGroup/buttonGroup';
 import '../styles/main.scss';
 
 const init = () => {
   const allStudents = studentData.getStudents();
   navbar.loadNavbar();
   studentList.createStudentList(allStudents);
+  buttonGroup.createHouseButtonGroup();
 };
 
 init();
